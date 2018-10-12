cfa20d810df47de091a81c5f1e3ba190730c55d2
Matt Shields <matthew.duncan.shields@gmail.com>
2018-10-12 10:13:34 -0700

    initial commit, index file begun

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from ('axios');
+import express from ('express');
+import bodyParser from ('body-parser');
+import massive from ('massive');
+// import * as serviceWorker from './serviceWorker';
+const app = express ();
+app.use(boydPArser.json());
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
